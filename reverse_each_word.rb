5f3c7d40b98f7d1ed170a2fe333edc864b7d6bf1
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-07 22:28:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,11 @@
-
+def reverse_each_word(string)
+  sentence = string.split(" ")
+  reversed = []
+  sentence.each do |str| 
+    reversed << str.reverse
+end
+  return reversed 
+end
 
 
 def reverse_each_word(string)
