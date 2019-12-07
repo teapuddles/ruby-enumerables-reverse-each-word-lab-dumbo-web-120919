0abe6210758a624670047666e06ac773f966c8ad
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-07 22:30:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,4 @@ end
   return reversed.join(" ")
 end
 
-def reverse_each_word(string)
-  sentence = string.split(" ")
-  sentence.collect do |str| 
-    str.reverse
-end
-  return sentence.join(" ")
-end
 
